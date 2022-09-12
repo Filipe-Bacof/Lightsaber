bf765462ef73caa73d21256b3f99eb03e6a6aa1f
Filipe Bacof <filipebacof@gmail.com>
2022-09-12 13:55:34 -0300

    Sons de Sabre adicionados

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ let light = document.querySelector('.light');
 
 function activate() {
     if (light.style.display === 'none') {
-        light.style.display = 'block'
+        light.style.display = 'block';
+        abrir.play();
     } else {
-        light.style.display = 'none'
+        light.style.display = 'none';
+        fechar.play();
     }
 }
+
+
+const abrir = new Audio('abre.mp3');
+const fechar = new Audio('fecha.mp3');
